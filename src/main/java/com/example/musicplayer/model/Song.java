79b8d3f384bcfdf9720eeb228317f3886eb3c592
lauraoliveri <lauraoliveri2003@gmail.com>
2025-12-05 19:29:51 +0100

    Create Song entity and add validation dependencies

---
+++ src/main/java/com/example/musicplayer/model/Song.java
@@ -0,0 +1,55 @@
+package com.example.musicplayer.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Song {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank // app side
+    @Column(nullable = false) // db side
+    private String title;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String artist;
+
+    private String audioUrl;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAudioUrl() {
+        return audioUrl;
+    }
+
+    public void setAudioUrl(String audioUrl) {
+        this.audioUrl = audioUrl;
+    }
+}
