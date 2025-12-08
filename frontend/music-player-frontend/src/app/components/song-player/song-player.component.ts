831103e326ea92e3ae52d369162ef27dd19a159e
lauraoliveri <lauraoliveri2003@gmail.com>
2025-12-08 20:27:22 +0100

    Feat: add basic components: song-list, song-form, song-player

---
+++ frontend/music-player-frontend/src/app/components/song-player/song-player.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-song-player',
+  imports: [],
+  templateUrl: './song-player.component.html',
+  styleUrl: './song-player.component.scss'
+})
+export class SongPlayerComponent {
+
+}
