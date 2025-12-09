3ddba43bf4058c3591f5e7d412abd77b73a271f0
lauraoliveri <lauraoliveri2003@gmail.com>
2025-12-09 14:58:33 +0100

    Feat: connect song-list component to backend

--- frontend/music-player-frontend/src/app/components/song-list/song-list.component.ts
+++ frontend/music-player-frontend/src/app/components/song-list/song-list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { SongService } from '../../services/song.service';
+import { Song } from '../../models/song.model'; // se hai creato un modello
+import { error } from 'console';
+
 
 @Component({
   selector: 'app-song-list',
@@ -8,4 +12,49 @@ import { Component } from '@angular/core';
 })
 export class SongListComponent {
 
+  constructor(private songService: SongService) { }
+
+  songs: Song[] = [];
+  currentSong?: Song;
+
+  // when component is initialized
+  ngOnInit(): void {
+
+    // Calls service to get songs
+    this.songService.getSongs().subscribe({
+      next: (data) => this.songs = data, // if successfull put data in songs array
+      error: (err) => console.error('Error loading songs', err) // error when not successfull
+    });
+  }
+
+  // to select a song to play
+  getSong(song: Song): void {
+    this.currentSong = song;
+  }
+
+  // add new song to list
+  addSong(newSong :Song) :void {
+    this.songService.addSong(newSong).subscribe({
+      next: (createdSong) => {  // if successfull add new song
+        this.songs.push(createdSong);
+      },
+      error: (err) => {  // error when not successfull
+        console.error('Error creating song', err)
+      },
+    })
+  }
+
+  // to delete a song
+  deleteSong(id?: number): void {
+    if (!id) return;
+    this.songService.deleteSong(id).subscribe({
+      next: () => { // when song is deleted update list
+        this.songService.getSongs().subscribe({
+          next: (data) => this.songs = data, // if successfull put data in songs array
+          error: (err) => console.error('Error loading songs', err) // error when not successfull
+        });
+      },
+      error: (err) => console.error('Error in deleting song', err)
+    });
+  }
 }
