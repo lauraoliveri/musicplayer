8e6867e2c887423527254b564cbe77024454ccfa
lauraoliveri <lauraoliveri2003@gmail.com>
2025-12-16 18:00:50 +0100

    feat: add routes paths

--- frontend/music-player-frontend/src/app/app.routes.ts
+++ frontend/music-player-frontend/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { SongListComponent } from './components/song-list/song-list.component';
+import { SongFormComponent } from './components/song-form/song-form.component';
+import { SongPlayerComponent } from './components/song-player/song-player.component';
 
 export const routes: Routes = [
     { path: 'songs', component: SongListComponent },
-    { path: '', redirectTo: 'songs', pathMatch: 'full' }
+    { path: '', redirectTo: 'songs', pathMatch: 'full' },
+    { path: 'addsongs', component: SongFormComponent },
+    { path: 'player/:title', component: SongPlayerComponent }
 ];
