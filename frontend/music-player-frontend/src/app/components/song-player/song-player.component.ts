2f07c8406bbec8e6afdc68f1fa519fd349b13d62
lauraoliveri <lauraoliveri2003@gmail.com>
2026-01-16 18:47:48 +0100

    feat: implements progress bar

--- frontend/music-player-frontend/src/app/components/song-player/song-player.component.ts
+++ frontend/music-player-frontend/src/app/components/song-player/song-player.component.ts
@@ -15,12 +15,15 @@ export class SongPlayerComponent {
   songs: Song[] = [];
   currentIndex: number = 0;
   currentSong: Song | null = null;
-
-
-
+  isExpanded = false; // mobile toggle
+  isPlaying = false; // play and pause buttons
+  progress = 0; // progressbar
+   
+  @ViewChild('audioRef') audio!: ElementRef<HTMLAudioElement>;
 
   constructor(private songService: SongService) { }
 
+  // lifecycle: runs after constructor, for initial data loading
   ngOnInit(): void {
 
     // uploads all songs 
@@ -47,51 +50,56 @@ export class SongPlayerComponent {
     });
   }
 
+  // lifecycle: runs after view is fully initialized
+  ngAfterViewInit(){
+    const audio = this.audio.nativeElement;
 
-  isExpanded = false;
+    // progress bar
+    audio.addEventListener('timeupdate', () => { 
+    this.progress = (audio.currentTime / audio.duration) * 100;
+
+    // when songs ends next songs plays
+    audio.addEventListener('ended', () => {
+    this.next();
+  });
+  });
+  }
 
   // to open or close player
   togglePlayer() {
     this.isExpanded = !this.isExpanded;
   }
 
-  // play and pause buttons
-  isPlaying = false;
-
-  @ViewChild('audioRef') audio!: ElementRef<HTMLAudioElement>;
-
+  // play pause button
   playPause() {
-    const audio = this.audio.nativeElement;
 
     if (this.isPlaying) {
-      audio.pause();
+      this.audio.nativeElement.pause();
       this.isPlaying = false;
     } else {
-      audio.play();
+      this.audio.nativeElement.play();
       this.isPlaying = true;
     }
   }
 
-  // next button
+  // next song button
   next() {
     if (this.songs.length === 0) {
       return;
     }
 
     this.currentIndex = (this.currentIndex + 1) % this.songs.length; // % is to loop
-
     const nextSong = this.songs[this.currentIndex];
     this.songService.selectSong(nextSong);
   }
 
-  // previous button
+  // previous song button
   previous() {
     if (this.songs.length === 0) {
       return;
     }
 
     this.currentIndex = (this.currentIndex - 1 + this.songs.length) % this.songs.length; // % is to loop
-
     const previousSong = this.songs[this.currentIndex];
     this.songService.selectSong(previousSong);
   }
